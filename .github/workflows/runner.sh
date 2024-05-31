e38fc09837bb14e5fcb7a8ca1508b2561ee12200
Wisp <1343347+wispborne@users.noreply.github.com>
2024-05-31 10:20:24 -0400

    Github actions & VC updates.

--- .github/workflows/runner.sh
+++ .github/workflows/runner.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 ### Edit
-# Your mod name. The version number will be attached to this to form "My-Mod-1.0.0"
-MOD_FOLDER_NAME=$(head -n 1 './mod-folder-name.txt')
-echo "Folder name without version will be $MOD_FOLDER_NAME"
+# Your mod name. The version number will be attached to this to form "My-Mod"
+MOD_FOLDER_NAME="TakeNoPrisoners"
+echo "Folder name will be $MOD_FOLDER_NAME"
 ###
 
 
